725bfc9762eb0581e74e7a9d3a520ac4798cc774
Dcruzz952 <115687948+Dcruzz952@users.noreply.github.com>
2023-07-25 16:07:43 +0530

    Update helloworld.py

--- Assignment/helloworld.py
+++ Assignment/helloworld.py
@@ -1,1 +1,9 @@
-print('Hello World', flush=True)
+import time
+import random
+
+list_text = ["Hello World","log_details","concurrent"]
+end_time = time.time() + 180
+while time.time() < end_time:
+        word = random.choice(list_text)
+        print(word)
+        time.sleep(1)  
